72c514356cf97cabd2dfdcf0e2be39d99ad6e30c
felix <felixsanchez0124@gmail.com>
2020-02-19 19:36:03 -0600

    converts string to array

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,15 @@
 #include <Arduino.h>
 
+int plusA;
 
-int randNum;
 
-String addcharToArr(String array, String _char){
-  for(int i = 0; i <= array.length();){
-    if( _char != String(array.charAt(i))){
-      if (i == array.length()){
-        array.concat(_char);
-        //Serial.print("caracter agregado: ");
-        //Serial.println(_char);
-        //Serial.print("el array hasta el momento es: ");
-        //Serial.println(array);
-        //Serial.println("array realmente returnado");
-        break;
-      }
-      i++;
-    }
-    else{
-      Serial.println("el caracter que quiere agregar ya esta agregado");
-      break;
-    }
-  }
 
-  return array;
-  
-}
 
-String RandNums(int max){
-  String sRandNums = String(random(1,5));
-  while(sRandNums.length() < max) {
-    sRandNums = addcharToArr(sRandNums, String(random(1, 5)));
-    Serial.println(sRandNums);
-  }
-
-  return sRandNums;
-}
 
 void setup() {
-  Serial.begin(9600);
-  //Serial.println(RandNums(4));
+
 }
+
 void loop() {
-  delay(2000);
-  Serial.println(RandNums(4));
 
 }
-
