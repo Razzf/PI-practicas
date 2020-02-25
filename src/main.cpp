b2b7c7575ebe40712ed7d7b598d0fe450fbc90a1
felix <felixsanchez0124@gmail.com>
2020-02-24 20:12:13 -0600

    funcs moved

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,60 @@ byte Cyl1Push = 2, Cyl2Push = 3, Cyl3Push = 4, Cyl4Push  = 5, Cyl1Pull = 6, Cyl2
 int numElements;
 String arr[20];
 
+void fillArr(String estring) {
+  for (int i = 0;+i < +estring.length()/2;i++){
+    arr[i] = estring.substring(0 + i*2,2 + i*2);
+  }
+}
+
+void CheckSecuence(String arrf[],int numElments) {
+  int counter = 0;
+
+  for(int i = 0;i < numElements;i++){
+    if (arrf[i] != ""){
+      counter++;
+    }
+  }
+  
+  for (int i = 0; i < counter;) {
+
+    Serial.println(arrf[i].substring(0,1));
+
+    if (arrf[i].substring(0,1) == "A"){
+      if(arrf[i].substring(1,2) == "+"){
+
+      }
+      else if (arrf[i].substring(1,2) == "-"){
+
+      }
+    }
+    else if (arrf[i].substring(0,1) == "B"){
+      if(arrf[i].substring(1,2) == "+"){
+
+      }
+      else if (arrf[i].substring(1,2) == "-"){
+
+      }
+    }
+    else if (arrf[i].substring(0,1) == "C"){
+      if(arrf[i].substring(1,2) == "+"){
+
+      }
+      else if (arrf[i].substring(1,2) == "-"){
+
+      }
+    }
+    else if (arrf[i].substring(0,1) == "D"){
+      if(arrf[i].substring(1,2) == "+"){
+
+      }
+      else if (arrf[i].substring(1,2) == "-"){
+
+      }
+    }
+  }
+}
+
 void setup() {
   String secExample = "A+A-B+C+B-C-";
   
@@ -19,34 +73,5 @@ void setup() {
   CheckSecuence(arr, numElements);
 }
 
-void loop() {
-  
 
-}
 
-void CheckSecuence(String arrf[],int numElments) {
-  int counter = 0;
-  int Cyl1, Cyl2, Cyl3, Cyl4;
-  for(int i = 0;i < numElements;i++){
-    if (arrf[i] != ""){
-      counter++;
-    }
-  }
-  
-  for (int i = 0; i < counter; i++) {
-    int Cyl;
-    String minusorplus;
-
-    //Serial.println(arrf[i].substring(0,1));
-
-    if (arrf[i].substring(0,1) == "B"){
-      Serial.println("cacas");
-    }
-  }
-}
-
-void fillArr(String estring) {
-  for (int i = 0;i < estring.length()/2;i++){
-    arr[i] = estring.substring(0 + i*2,2 + i*2);
-  }
-}
